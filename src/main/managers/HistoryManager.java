ac7c7970df31e3fc8b0cba8640fc833a880761ff
berbenets_m <berbenets.maksim@bk.ru>
2025-02-15 00:36:50 +0300

    первая проверка ФЗ6

--- src/main/managers/HistoryManager.java
+++ src/main/managers/HistoryManager.java
@@ -5,7 +5,7 @@ import main.tasks.Task;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+
 
 public interface HistoryManager {
     public void addTask(Task task);
