f9f4ac4f45aecc4328bb80cbfabc692b536d94ff
berbenets_m <berbenets.maksim@bk.ru>
2025-03-20 18:12:59 +0300

    ФЗ9

--- src/main/HttpTaskServer.java
+++ src/main/HttpTaskServer.java
@@ -1,35 +1,22 @@
 package main;
 
 
-import main.constants.Status;
-import main.exceptions.ManagerSaveException;
-import main.managers.FileBackedTaskManager;
-import main.tasks.Epic;
-import main.tasks.Subtask;
+import com.sun.net.httpserver.HttpServer;
 
-import java.io.File;
 import java.io.IOException;
-import java.time.Duration;
-import java.time.LocalDateTime;
+import java.net.InetSocketAddress;
 
 
 public class HttpTaskServer {
     private static final int PORT = 8080;
-    public static void main(String[] args) throws ManagerSaveException, IOException {
-        File file = File.createTempFile("practicum", ".csv");
-        FileBackedTaskManager fb = new FileBackedTaskManager(file);
-        Epic epic1 = new Epic("Эпик 1", "Описание эпика 1");
-        fb.addEpic(epic1);
-        System.out.println(epic1);
-        Subtask subtask1 = new Subtask("Подзадача1", "Описание подзадачи1", Status.DONE, epic1.getId(),
-                Duration.ofMinutes(10), LocalDateTime.of(2025, 1, 10, 10, 0));
-        Subtask subtask2 = new Subtask("Подзадача2", "Описание подзадачи2", Status.DONE, epic1.getId(),
-                Duration.ofMinutes(10), LocalDateTime.of(2025, 1, 10, 10, 20));
-        fb.addSubtask(subtask1);
-        fb.addSubtask(subtask2);
-        System.out.println(epic1);
-        FileBackedTaskManager newFile = FileBackedTaskManager.loadFromFile(file);
-        System.out.println(newFile.getAllEpics());
-        System.out.println(newFile.getAllSubtasks());
+
+    public static void main(String[] args) throws IOException {
+        HttpServer httpServer = HttpServer.create();
+        httpServer.bind(new InetSocketAddress(PORT), 0); // связываем сервер с сетевым портом
+        httpServer.createContext("/tasks", new );
+        httpServer.createContext("/subtasks", new );
+        httpServer.createContext("/tasks", new );
+        httpServer.createContext("/tasks", new );
+        httpServer.createContext("/tasks", new );
     }
 }
